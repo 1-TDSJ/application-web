a06d88ed5a3665942a68113fda9a0844de7135f3
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2020-09-23 10:00:25 -0300

    atualizando exercício cadastro-01

--- controle-01/src/br/com/fiap/bean/Cliente.java
+++ controle-01/src/br/com/fiap/bean/Cliente.java
@@ -17,4 +17,66 @@ public class Cliente {
 		// TODO Auto-generated constructor stub
 	}
 
+	public Cliente(int idCli, String nome, String sobrenonme, Date dataNasc, char genero, long telefone) {
+		super();
+		this.idCli = idCli;
+		this.nome = nome;
+		this.sobrenonme = sobrenonme;
+		this.dataNasc = dataNasc;
+		this.genero = genero;
+		this.telefone = telefone;
+	}
+
+	public int getIdCli() {
+		return idCli;
+	}
+
+	public void setIdCli(int idCli) {
+		this.idCli = idCli;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenonme() {
+		return sobrenonme;
+	}
+
+	public void setSobrenonme(String sobrenonme) {
+		this.sobrenonme = sobrenonme;
+	}
+
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+	public char getGenero() {
+		return genero;
+	}
+
+	public void setGenero(char genero) {
+		this.genero = genero;
+	}
+
+	public long getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(long telefone) {
+		this.telefone = telefone;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
 	}
--- controle-01/src/br/com/fiap/dao/ClienteDAO.java
+++ controle-01/src/br/com/fiap/dao/ClienteDAO.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.fiap.bean.Cliente;
@@ -34,19 +37,39 @@ public class ClienteDAO {
 			//Criando uma instância de Cliente
 			Cliente cli = null;
 			
+			//Criando a lista que vai ser retornada para o BO.
+			List<Cliente> listagem = new ArrayList<Cliente>();
+			
 			while (rs.next()) {
-				//A cada nova iteração um novo oobjeto é criado
+				//A cada nova iteração um novo objeto é criado
 				cli = new Cliente(); 
 				
 				//Agora adicione os dados ao objeto
+				cli.setIdCli(rs.getInt(1));
+				cli.setNome(rs.getString(2));
+				cli.setSobrenonme(rs.getString(3));
+				SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
+				cli.setDataNasc(sf.parse(rs.getString(4)));
+				cli.setGenero(rs.getString(5).charAt(0));
+				cli.setTelefone(Long.parseLong(rs.getString(6)));
 				
-				
+				//Adicionando o objeto na lista.
+				listagem.add(cli);
 			}
+			
+			ps.close();
+			rs.close();
+			con.close();
+			
+			return listagem;
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
 		}
 		
+		return null;
 	}
 	
 
